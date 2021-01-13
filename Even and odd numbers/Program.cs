1621eee3e5ca5d620d340564e8d586bbf07a622d
Aarox3 <poiuytrewq1990@o2.pl>
2021-01-13 23:24:00 +0100

    Add project files.

---
+++ Even and odd numbers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_and_odd_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj");
+            while (true)
+            {
+
+          
+            try 
+            { 
+                
+                Console.WriteLine("Wprowadź proszę dowolną liczbę. Program poda komunikat, czy jest to liczba parzysta bądź nieparzysta.");
+
+                int userNumber = int.Parse(Console.ReadLine());
+
+
+
+                 if (userNumber%2==0)
+                     {
+                        Console.WriteLine("Liczba " + userNumber + " jest parzysta.");
+                     }
+
+                 if (userNumber%2==1)
+                     {
+                         Console.WriteLine("Liczba " + userNumber + " jest nieparzysta.");
+                     }
+                    break;
+            }
+                
+                                 
+            catch (Exception ex)
+                {
+                    Console.WriteLine("Podana wartość jest niewłaściwa!");
+                }
+                
+            }
+        }
+        }
+        }
+   
